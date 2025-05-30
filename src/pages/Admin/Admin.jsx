0183ab2b0107b14705eccb566cdfb92e6defbed1
Wuyy <23520605@gm.uit.edu.vn>
2025-05-30 20:12:18 +0700

    30/5/2025

---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import MyFooter from '@components/Footer/Footer';
+import MyHeader from '@components/Header/Header';
+import MainLayout from '@components/Layout/Layout';
